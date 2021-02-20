1991c1cb10508179ea5785d721bf92d20951861e
Samuelangeles <samueljoseangelereye@gmail.com>
2021-02-20 15:17:05 -0600

    actualizacion de datos

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,16 @@ const usuario = {
     activo: true,
     fechaNaci: 0
 }
-
-db.collection('usuarios')
+const usuarioRef = db.collection('usuarios');
+/*db.collection('usuarios')
 .add(usuario)
+.then(docRef => {
+    console.log(docRef )
+})
+.catch(e => console.log('error', e))*/
+
+usuarioRef 
+     .doc('01')
+     .update({
+         activo: false
+     })
