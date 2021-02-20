78ea28db584764655287aa1cc361650aa70ee2ad
Samuelangeles <samueljoseangelereye@gmail.com>
2021-02-20 08:59:25 -0600

    creacion de tabla

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
-import db from './farebase/config';
+import db  from './farebase/config';
 
-const usuario ={
-    nombre: 'Samuel',
-    Activo: true,
+const usuario={
+    nombre: 'samuel',
+    activo: true,
     fechanaci: 0
-
 }
+
 db.collection('usuarios')
-  .add( usuario )
+.add( usuario )
+
