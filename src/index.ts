62eb8f7ce1cbf55a6e70ebf260b7c310551946de
Samuelangeles <samueljoseangelereye@gmail.com>
2021-02-21 10:54:34 -0600

    primer video instalaccion de npm

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,2 @@
-import db from './firebase/config';
-
-const usuario = {
-    nombre: 'Samuel',
-    activo: true,
-    fechaNaci: 0
-}
-const usuarioRef = db.collection('usuarios');
-/*db.collection('usuarios')
-.add(usuario)
-.then(docRef => {
-    console.log(docRef )
-})
-.catch(e => console.log('error', e))*/
-
-usuarioRef 
-     .doc('01')
-     .update({
-         activo: false
-     })
+const mensaje= 'Hola mundo';
+console.log(mensaje);
