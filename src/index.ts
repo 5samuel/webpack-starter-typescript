39053e5a6147463e47fc98f34f8876d8226cb4e0
Samuelangeles <samueljoseangelereye@gmail.com>
2021-02-21 20:17:14 -0600

    insertar datos

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-import './firebase/config';
+import db from './firebase/config';
+const usuario={
+    nombre: 'Samuel',
+    activo:true,
+    fechaNaci:0
+}
+db.collection('usuarios')
+ .add(usuario)
+ .then(docRef=> {
+     console.log(docRef)
+ })
+ .catch(e => console.log('error', e))
