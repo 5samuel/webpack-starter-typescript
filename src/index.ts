acba98e4345a942cdff71bd085fc25a1db75116e
samuelangeles <samueljoseangelereye@gmail.com>
2021-03-01 13:50:21 -0600

    utilizacion de orderBy

--- src/index.ts
+++ src/index.ts
@@ -1,43 +1,11 @@
 import db from './firebase/config';
 import {retronarDocumento} from './herpers/mostrar-documentos';
 const usuario={
-    nombre: 'Samuel',
+    nombre: 'melisa',
     activo:true,
-    fechaNaci:0
+    fechaNaci:47853
 }
-//actualizacion 
+
 const usuarioRef= db.collection('usuarios');
-/*db.collection('usuarios')
- .add(usuario)
- .then(docRef=> {
-     console.log(docRef)
- })
- .catch(e => console.log('error', e))*/
- /*usuarioRef 
- .doc('DcHteafFhd0dg6')
- .update({
-     activo:false
- })*/
- //borrado de impformacion 
- /*usuarioRef
- .doc('RDG6dgg6sGHFhf7d')
- .delete()
- .then (() => console.log('borrado'))
- .catch(e => console.log('error', e));*/
-
- /*usuarioRef
- .onSnapshot( snap => {
-    retronarDocumento(snap);
-    
- })*/
-
- //usuarioRef.where('Salario', '>',1800 ).get().then( retronarDocumento);
-
-/* usuarioRef.where('Salario', '>', 1800 )
- .where('usuario', '<', 2300)
- .get().then( retronarDocumento);*/
-
- 
- usuarioRef.where('Salario', '>', 1800 )
- .where('uactivo','==', true)
- .get().then( retronarDocumento);
+usuarioRef.orderBy('usuario')
+    .get().then(retronarDocumento)
